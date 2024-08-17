7e0008ef0931b3ea6ba87b9daba20cbb6ad1cc51
Ahany42 <ahanyfathy42@gmail.com>
2024-08-17 18:14:45 +0300

    Get Comment and delete comment apis

--- index.js
+++ index.js
@@ -4,51 +4,47 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 let LoggedUserImg="https://i.ibb.co/Dr2ZcHQ/IMG-20231027-WA0082.jpg";
-app.get('/Users/LoggedInUser', (req, res) => {
+app.get('/users/loggedin', (req, res) => {
     res.json({userName: "Aly Hany", gender: "M",src:LoggedUserImg});
 });
-let data = [
-    {
-        id: 1,
-        UserName: "Aly Hany",
-        Comment: "This is a great post!",
-        Date: "Sep 25, 2024",
-        src:LoggedUserImg,  
-        Gender: "M"
-    },
-    {
-        id: 2,
-        UserName: "Bob",
-        Comment: "I found this article very informative.",
-        Date: "Sep 26, 2024",
-        Gender: "M"
-    },
-    {
-        id: 3,
-        UserName: "Charlie",
-        Comment: "I disagree with some points mentioned.",
-        Date: "Sep 27, 2024",
-        Gender: "M"
-    },
-    {
-        id: 4,
-        UserName: "Diana",
-        Comment: "Can you provide more details on this topic?",
-        Date: "Aug 04, 2024",
-        Gender: "F"
+let data = [];
+const initialLength = data.length;
+app.get('/comments/get/:id',(req,res)=>{
+    const id = parseInt(req.params.id,10);
+    if(!isNaN(id)){
+        const comment = data.find(comment=>comment.id===id);
+        if (comment) {
+            res.json(comment);
+        } else {
+            res.status(404).json({ error: 'Comment not found' });
+        }
     }
-];
-app.get('/Comments/GetAll', (req, res) => {
+    else{
+        return res.status(400).json({ error: 'ID parameter is missing' });
+    }
+
+})
+app.get('/comments/getall', (req, res) => {
     res.json(data);
-});
-app.post('/Comments/AddComment',(req,res)=>{
+})
+app.post('/comments/add',(req,res)=>{
 let comment = req.body;
-data.push(comment);
 if (!comment || Object.keys(comment).length === 0) {
     return res.status(400).send('No comment provided');
   }
   data.push(comment);
   res.status(201).send('Comment added successfully');
+})
+app.delete('/comments/delete/:id',(req,res)=>{
+    const deletedId = parseInt(req.params.id);
+    if(!isNaN(deletedId)){
+        data = data.filter(comment=>comment.id!==deletedId);
+    }
+    if (data.length < initialLength) {
+        res.status(200).send('Comment deleted successfully');
+      } else {
+        res.status(404).send('Comment not found');
+      }
 })
 app.listen(3004, () => {
     console.log("Server is running on port 3004");
