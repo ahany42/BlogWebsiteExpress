9e2177e164856f99b4ca997e793104e6847d5ddc
Ahany42 <ahanyfathy42@gmail.com>
2024-08-17 22:14:40 +0300

    adding id field to user

--- index.js
+++ index.js
@@ -5,9 +5,42 @@ app.use(cors());
 app.use(express.json());
 let LoggedUserImg="https://i.ibb.co/Dr2ZcHQ/IMG-20231027-WA0082.jpg";
 app.get('/users/loggedin', (req, res) => {
-    res.json({userName: "Aly Hany", gender: "M",src:LoggedUserImg});
+    res.json({id:1,userName: "Aly Hany", gender: "M",src:LoggedUserImg});
 });
-let data = [];
+let data = [
+    {
+        id: 2,
+        userName: "Samir Kamouna",
+        comment: "I found this article very informative.",
+        date: "Sep 26, 2024",
+        gender: "M",
+        postedBy:2
+    },
+    {
+        id: 3,
+        userName: "Mohamed Elneny",
+        comment: "I disagree with some points mentioned.",
+        date: "Sep 27, 2024",
+        gender: "M",
+        postedBy:3
+    },
+    {
+        id: 4,
+        userName: "Fatima Koshary",
+        comment: "Can you provide more details on this topic?",
+        date: "Aug 1, 2024",
+        gender: "F",
+        postedBy:4
+    },
+    {
+        id: 5,
+        userName: "Eneam Salousa",
+        comment: "Can you provide more details on this topic?",
+        date: "Aug 17, 2024",
+        gender: "F",
+        postedBy:4
+    }
+];
 const initialLength = data.length;
 app.get('/comments/get/:id',(req,res)=>{
     const id = parseInt(req.params.id,10);
