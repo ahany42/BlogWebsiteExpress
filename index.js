4ae7db29e4d1e1d1a78fbfc399cab48feb679410
Ahany42 <ahanyfathy42@gmail.com>
2024-08-17 15:57:54 +0300

    Add Comment Api

--- index.js
+++ index.js
@@ -1,48 +1,55 @@
 const express = require("express");
 const cors = require("cors"); 
-
 const app = express();
 app.use(cors());
+app.use(express.json());
 let LoggedUserImg="https://i.ibb.co/Dr2ZcHQ/IMG-20231027-WA0082.jpg";
-app.get('/LoggedInUser', (req, res) => {
+app.get('/Users/LoggedInUser', (req, res) => {
     res.json({userName: "Aly Hany", gender: "M",src:LoggedUserImg});
 });
-app.get('/Comments', (req, res) => {
-    const data = [
-        {
-            id: 1,
-            UserName: "Aly Hany",
-            Comment: "This is a great post!",
-            Date: "Sep 25, 2024",
-            src:LoggedUserImg,  
-            Gender: "M"
-        },
-        {
-            id: 2,
-            UserName: "Bob",
-            Comment: "I found this article very informative.",
-            Date: "Sep 26, 2024",
-            Gender: "M"
-        },
-        {
-            id: 3,
-            UserName: "Charlie",
-            Comment: "I disagree with some points mentioned.",
-            Date: "Sep 27, 2024",
-            Gender: "M"
-        },
-        {
-            id: 4,
-            UserName: "Diana",
-            Comment: "Can you provide more details on this topic?",
-            Date: "Aug 04, 2024",
-            Gender: "F"
-        }
-    ];
-
+let data = [
+    {
+        id: 1,
+        UserName: "Aly Hany",
+        Comment: "This is a great post!",
+        Date: "Sep 25, 2024",
+        src:LoggedUserImg,  
+        Gender: "M"
+    },
+    {
+        id: 2,
+        UserName: "Bob",
+        Comment: "I found this article very informative.",
+        Date: "Sep 26, 2024",
+        Gender: "M"
+    },
+    {
+        id: 3,
+        UserName: "Charlie",
+        Comment: "I disagree with some points mentioned.",
+        Date: "Sep 27, 2024",
+        Gender: "M"
+    },
+    {
+        id: 4,
+        UserName: "Diana",
+        Comment: "Can you provide more details on this topic?",
+        Date: "Aug 04, 2024",
+        Gender: "F"
+    }
+];
+app.get('/Comments/GetAll', (req, res) => {
     res.json(data);
 });
-
+app.post('/Comments/AddComment',(req,res)=>{
+let comment = req.body;
+data.push(comment);
+if (!comment || Object.keys(comment).length === 0) {
+    return res.status(400).send('No comment provided');
+  }
+  data.push(comment);
+  res.status(201).send('Comment added successfully');
+})
 app.listen(3004, () => {
     console.log("Server is running on port 3004");
 });
